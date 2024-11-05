754ce26fae824ae06865f801015ee7024e683e27
Geoff Fletcher <xov4m05a@students.codeinstitute.net>
2024-11-05 12:43:56 +0000

    add main functions in runGame

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
-    
+
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
@@ -17,7 +17,8 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function runGame() {
-
+    let num1 = Math.floor(Math.random()*25) + 1;
+    let num2 = Math.floor(Math.random()*25) + 1;
 }
 
 function checkAnswer() {
